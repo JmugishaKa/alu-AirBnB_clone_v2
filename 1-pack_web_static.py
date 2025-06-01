af350a8761e27bfdea22964ca7122e62128dbab9
JmugishaKa <j.mugisha2@alustudent.com>
2025-06-01 23:09:20 +0200

    Full deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-""" a fabric script to generate a .tgz archive from web_static folder"""
+"""Fabric script to generate a .tgz archive from the contents of the web_static folder"""
 from fabric.api import local
-from time import strftime
-from os.path import exists, isdir
-import os
+from datetime import datetime
 
 
 def do_pack():
-    """
-    Generates a .tgz file from the contents of the web_static folder
-    Returns:
-        file path when successful or none otherwise.
-
-    """
-    
-    date_time = strftime("%Y%m%d%H%M%S")
-    file_name = "web_static_{}.tgz".format(date_time)
-    file_path = "version/{}".format(file_name)
-
-    print("Packing web_static to {} web_static".format(file_path))
-    result = local("tar -cvzf {} web_static".format(file_path))
-    
-    if result.failed:
-        return none  
-
-    return file_name
-    except Exception as e:
-        return None
+    """Function to generate a .tgz archive from the contents of the web_static folder"""
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date)
+    result = local("tar -cvzf {} web_static".format(file_name))
+    if result.succeeded:
+        return file_name
+    return None
